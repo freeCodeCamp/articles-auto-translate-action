71ac873c8676ae8951a855da064386aac7f0c904
budblack <budblack@qq.com>
2024-07-31 10:24:52 +0000

    chore: Update task_auto_translate_step_01B_read_articel to extract author information

--- src/tasks/task_auto_translate_step_01B_read_articel.ts
+++ src/tasks/task_auto_translate_step_01B_read_articel.ts
@@ -17,15 +17,16 @@ export async function task_auto_translate_step_01B_read_articel(options: main_op
      *************************************************************************************************************************************/
 
     const str_md = await readFile(with_orginal_markdown_file_path, 'utf-8');
-    const arr_str_md = str_md.split('\n\n');
-    let str_title = '', str_date = '', str_author_url = '', str_original_url = '', str_translator = '', str_reviewer = '';
+    const arr_str_md = str_md.split('\n');
+    let str_title = '', str_date = '', str_author = '', str_author_url = '', str_original_url = '', str_translator = '', str_reviewer = '';
     let count_flag_scan = 0;
     for (let i = 0; i < arr_str_md.length; i++) {
-        const str = arr_str_md[i];
-        if (str === '---') { count_flag_scan += 1 }
+        const str = arr_str_md[i].trim();
+        if (str.startsWith('---')) { count_flag_scan += 1 }
         if (count_flag_scan === 1) {
             if (str.startsWith('title: ')) str_title = str.replace('title: ', '').trim();
             else if (str.startsWith('date: ')) str_date = str.replace('date: ', '').trim();
+            else if (str.startsWith('author: ')) str_author = str.replace('author: ', '').trim();
             else if (str.startsWith('authorURL: ')) str_author_url = str.replace('authorURL: ', '').trim();
             else if (str.startsWith('originalURL: ')) str_original_url = str.replace('originalURL: ', '').trim();
             else if (str.startsWith('translator: ')) str_translator = str.replace('translator: ', '').trim();
@@ -43,6 +44,7 @@ export async function task_auto_translate_step_01B_read_articel(options: main_op
     const original_meta = {
         title: str_title,
         date: str_date,
+        author: str_author,
         authorURL: str_author_url,
         originalURL: str_original_url,
         translator: str_translator,
