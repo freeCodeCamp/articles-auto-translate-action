4a1f0391831f483846a755cd047cf7a108d4b8f0
yoko <25644062+sidemt@users.noreply.github.com>
2024-08-27 15:18:03 +0900

    fix: improve Japanese prompt (#6)

--- src/tasks/task_auto_translate_step_02_trans_articles.ts
+++ src/tasks/task_auto_translate_step_02_trans_articles.ts
@@ -7,7 +7,7 @@ import { ensureFile, readFile, writeFile } from "fs-extra";
 // simply to align with the codes used in other parts of our platform.
 const map_str_prompts: any = {
     "zh": "我有段 md 文件，请翻译为中文。翻译需要严格保留源文件 markdown 排版布局，请直接输出，不要在作询问。\n",
-    "ja": "この Markdown を日本語に翻訳してください。逐語訳ではなく、日本語の IT 系ウェブメディアの記事として自然な表現にしてください。Markdown の書式は、原文から変更しないでください。また、半角英数字と日本語の文字の間には半角スペースを入れてください。追加の質問はせず、直接出力してください。\n",
+    "ja": "この Markdown を日本語に翻訳してください。逐語訳ではなく、日本語の IT 系ウェブメディアの記事として自然な表現にしてください。Markdown の書式は、原文から変更しないでください。また、半角英数字と日本語の文字の間には半角スペースを入れ、コロン: と丸括弧 () は半角に統一してください。追加の質問はせず、直接出力してください。\n",
     "es": "Tengo un archivo md, por favor tradúzcalo al español. La traducción debe mantener estrictamente el formato y la disposición del archivo original en markdown. Por favor, simplemente muéstrelo sin hacer preguntas.\n",
     "pt": "Eu tenho um arquivo md, por favor, traduza-o para o português. A tradução deve manter rigorosamente a formatação e layout markdown do arquivo original. Por favor, forneça a tradução diretamente sem fazer perguntas."
 }
